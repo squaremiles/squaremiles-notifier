222cd33dcbaffdabcefc0eb51516f95497a68b00
neelambari123 <82968999+neelambari123@users.noreply.github.com>
2024-01-31 10:58:08 +0530

    added for annatation

--- src/main/java/com/db/squaremile/ObservedAspectConfig.java
+++ src/main/java/com/db/squaremile/ObservedAspectConfig.java
@@ -1,23 +1,32 @@
 package com.db.squaremile;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.client.RestTemplate;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class ObservedAspectConfig {
+	
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+	    @Value("${spring.application.name}") String applicationName) {
+	    return (registry) -> registry.config().commonTags("application", applicationName);
+	}
 
 	@Bean
 	public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
 		observationRegistry.observationConfig().observationHandler(new LogObservationHandler());
 		return new ObservedAspect(observationRegistry);
 	}
-
+	
 	/*
 	 * @Bean public TimedAspect timedAspect(MeterRegistry registry) { return new
 	 * TimedAspect(registry); }
--- src/main/java/com/db/squaremile/controller/NotificationController.java
+++ src/main/java/com/db/squaremile/controller/NotificationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.db.squaremile.dto.PropertyDto;
 import com.db.squaremile.service.NotificationService;
 
+import io.micrometer.observation.annotation.Observed;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -21,7 +22,7 @@ public class NotificationController {
 	private NotificationService notificationService;
 	
 	@PostMapping(value="/notify",consumes = MediaType.APPLICATION_JSON_VALUE)
-	//@Observed(name = "send.notification.for.property.controller", lowCardinalityKeyValues = {"send.notification", "send.notification"})
+	@Observed(name = "send.notification.property.controller", lowCardinalityKeyValues = {"send.notification", "send.notification"})
 	public ResponseEntity<String> sendNotificationForProperty(@RequestBody PropertyDto proprtyDto) {
 		log.info("notifier is getting called");
 		return new ResponseEntity(notificationService.sendNotification(proprtyDto),HttpStatus.OK);
